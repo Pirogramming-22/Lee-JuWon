ded59874952bd74c248be9e16ef17d3f167b8c93
leexzu1 <jackie20029655@gmail.com>
2025-01-09 01:27:57 +0900

    Django 폼(수정) & CSS 최종

--- djangogirls/myvenv/blog/views.py
+++ djangogirls/myvenv/blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
-from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -26,10 +25,11 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
